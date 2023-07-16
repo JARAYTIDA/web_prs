dc9db187c375edf3315b058ac1a4e51bce8d0dfa
JARAYTIDA <rajadityakumawat@gmail.com>
2023-07-16 09:45:35 +0530

    search by id

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,12 +1,13 @@
 import  express  from "express";
 
 // local imports
-import { getPosts, createPost } from "../controllers/posts.js";
+import { getPosts, getPost, createPost } from "../controllers/posts.js";
 
 
 const router = express.Router();
 
 router.get('/', getPosts);
+router.get('/:id', getPost);
 router.post('/', createPost);
 
 export default router;
